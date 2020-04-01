d6628eaf4177a909cbb71455fdb3593f228626a9
Jacob Hall <halljaco@yahoo.com>
2020-04-01 17:15:23 -0600

    first commit basics on screen no functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,89 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './reset.css'
 import './App.css';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  
+  render(){
+    return (
+      <div className="App">
+
+          
+
+        <section className={"mainComponent"}>
+            <header>
+              <div className={"startBootstrap"}>Start Bootstrap</div>
+
+              <nav>
+                  <ul>
+                    <li>SERVICES</li>
+                    <li>PORTFOLIO</li>
+                    <li>ABOUT</li>
+                    <li>TEAM</li>
+                    <li>CONTACT</li>
+                  </ul>
+              </nav>
+              
+            </header>
+
+            <h2>Welcome To Our Studio!</h2>
+            <h1>IT'S NICE TO MEET YOU</h1>
+            <button>TELL ME MORE</button>
+
+
+        </section>
+
+      </div>
+    );
+  }
 }
 
+
 export default App;
+
+
+
+// ------------------------------->  layout if I were to do this for my NoDB project <---------------
+
+
+// import React, {Component} from 'react';
+
+// class App extends Component {
+//   constructor(props){
+//     super(props);
+
+//     this.state = {
+
+//     }
+//     // bind here when necessary
+//   }
+
+
+
+//   render(){
+  
+//     return(
+//       <div>
+        
+      
+  
+//   {/* are these supposed to be more of a file path format? */}
+//           <MainComponent />
+//           <Services />
+//           <Portfolio />
+//           <About />
+//           <Team />
+//           <Contact />
+  
+  
+//       </div>
+//     )
+//   }
+
+// }  
+
+// export default App;
+
+
